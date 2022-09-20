e0ebc0375022cb248c99570d64be36eafad96a5d
Kevin McGur <kevin.mcgurn@mac.com>
2022-09-20 12:16:46 -0400

    background added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// git add .
+// git commit -m "first commit"
+// git@github.com:CaptivateLLC/captivate-ad-preview.git
+
+// git remote add origin git@github.com:CaptivateLLC/captivate-ad-preview.git
+// git push origin dev
+
+// git remote set-url origin git@github.com:CaptivateLLC/captivate-ad-preview.git
+
 import React, { useState, useEffect, useMemo, useRef } from "react";
 import styles from "./App.scss";
 import Header from "./components/Header";
 import Dropzone from "./components/Dropzone";
 import AssetDisplay from "./components/AssetDisplay";
 import DroppedFileAttributes from "./components/DroppedFileAttributes";
+import BackgroundImages from "./components/BackgroundImages";
 
 const displayContainer = {
-  width: "100%",
+  // position: "absolute",
   display: "flex",
   flexWrap: "wrap",
 };
@@ -24,12 +34,13 @@ function App() {
 
   return (
     <div className="App">
+      <BackgroundImages></BackgroundImages>
       <Header></Header>
       <DroppedFileAttributes droppedFile={droppedFile}></DroppedFileAttributes>
-      <Dropzone handleDropzoneChanges={handleDropzoneChanges}></Dropzone>
-      <div style={displayContainer}>
+      <div className="dropContainer" style={displayContainer}>
         <AssetDisplay label="Large Format Display" droppedFile={droppedFile} width={768}></AssetDisplay>
         <AssetDisplay label="Elevator" droppedFile={droppedFile} width={480}></AssetDisplay>
+        <Dropzone handleDropzoneChanges={handleDropzoneChanges}></Dropzone>
       </div>
     </div>
   );
