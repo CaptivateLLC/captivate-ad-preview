2bf2ef0bebb5bb9f89d3f3a35b9fd533f3af9a11
Kevin McGur <kevin.mcgurn@mac.com>
2022-09-27 10:49:52 -0400

    trying to load ffmpeg v396.95

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import ErrorMessage from "./components/ErrorMessage";
 import Footer from "./components/Footer";
 import separator from "./assets/separator.svg";
 
-// import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
 function App() {
   const [droppedFile, setDroppedFile] = useState({});
@@ -26,15 +26,15 @@ function App() {
   const [errorMessageString, setErrorMessageString] = useState();
   const [errorTextAnimationRun, setErrorTextAnimationRun] = useState(false);
 
-  // const ffmpeg = createFFmpeg({
-  //   log: true,
-  // });
-  // const load = async () => {
-  //   await ffmpeg.load();
-  // };
-  // useEffect(() => {
-  //   load();
-  // });
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+  const load = async () => {
+    await ffmpeg.load();
+  };
+  useEffect(() => {
+    load();
+  });
 
   const handleDropzoneChanges = (name, value) => {
     setDroppedFile((prevState) => ({
@@ -75,12 +75,12 @@ function App() {
 
   const removeAudioChannel = async (e) => {
     console.log("click3");
-    await ffmpeg.load();
-    // this works on command line:   ffmpeg -i "audio.mp4" -c copy -an "noaudio.mp4"
-    ffmpeg.FS("writeFile", "noAudio.mp4", await fetchFile(droppedFile.payload));
-    await ffmpeg.run("-framerate", "1/10", "-i", "noAudio.mp4", "-c:v", "libx264", "-t", "10", "-pix_fmt", "yuv420p", "-vf", "scale=1920:1080", "test.mp4");
-    const data = ffmpeg.FS("readFile", "test.mp4");
-    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" })));
+    // await ffmpeg.load();
+    // // this works on command line:   ffmpeg -i "audio.mp4" -c copy -an "noaudio.mp4"
+    // ffmpeg.FS("writeFile", "noAudio.mp4", await fetchFile(droppedFile.payload));
+    // await ffmpeg.run("-framerate", "1/10", "-i", "noAudio.mp4", "-c:v", "libx264", "-t", "10", "-pix_fmt", "yuv420p", "-vf", "scale=1920:1080", "test.mp4");
+    // const data = ffmpeg.FS("readFile", "test.mp4");
+    // setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" })));
   };
 
   return (
