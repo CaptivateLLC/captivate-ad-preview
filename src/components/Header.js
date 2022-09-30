c1e10227b1b5d62815d00e6728115e4692b0f82e
Kevin McGurn <kevin.mcgurn@mac.com>
2022-09-30 09:37:02 -0400

    moved text to top

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,11 @@ function Header() {
       <div className={styles.headerTextStyle}>Standard Ad Preview v5.2</div>
       <div className={`${styles.relativeCenterBody} ${styles.headerContainer}`}>
         <div className={styles.innerTextContainer}>
-          <div className={styles.text}>
+          <div className={styles.text22}>Drag and drop 16:9 creative anywhere or click a preview window to browse</div>
+          <div className={styles.text18}>
             <img src={blueCheck} className={styles.blueCheck}></img>For best results, stand 3 to 5 feet away from your screen
           </div>
-          <div className={styles.text}>
+          <div className={styles.text18}>
             <img src={blueCheck} className={styles.blueCheck}></img>All elements, especially text and QR codes, should be legible in both formats
           </div>
         </div>
